bfa5e5e1f56cd8123409910e7dcb71cb31d6268e
Max Korytko <maxk2005@gmail.com>
2020-02-21 07:45:51 -0500

    Code re-factoring

--- app/src/main/java/com/maxk/marvy/adapter/MarvelCharactersAdapter.kt
+++ app/src/main/java/com/maxk/marvy/adapter/MarvelCharactersAdapter.kt
@@ -15,7 +15,7 @@ class MarvelCharactersAdapter:
                      MarvelCharacterListItemBinding): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Character) {
-            binding.characterName.text = item.name
+            binding.character = item
             binding.imageView.image = item.thumbnail
         }
     }
