758896345b85ad090d5ca5e02e045ad85ce7d7f5
Max Korytko <maxk2005@gmail.com>
2020-04-13 21:46:10 -0400

    Implement expanding/collapsing character info

--- app/src/main/java/com/maxk/marvy/characters/view/MarvelCharacterExpandableInfoView.kt
+++ app/src/main/java/com/maxk/marvy/characters/view/MarvelCharacterExpandableInfoView.kt
@@ -1,10 +1,15 @@
 package com.maxk.marvy.characters.view
 
+import android.animation.LayoutTransition
 import android.content.Context
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import com.bumptech.glide.Glide
 import com.maxk.marvy.databinding.MarvelCharacterExpandableInfoViewBinding
 import com.maxk.marvy.extensions.layoutInflater
+import com.maxk.marvy.extensions.resolveAttribute
 import com.maxk.marvy.model.CharacterInfo
 
 class MarvelCharacterExpandableInfoView(context: Context, private val characterInfo: CharacterInfo)
@@ -24,9 +29,19 @@ class MarvelCharacterExpandableInfoView(context: Context, private val characterI
                 LayoutParams.MATCH_PARENT
             ))
 
-            expandImage.setOnContextClickListener {
+            val layoutTransition = LayoutTransition()
+            layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+            infoContainer.layoutTransition = layoutTransition
+
+            expandImage.setOnClickListener {
+                setExpanded(binding.infoContainer.height == 0)
                 true
             }
         }
+    }
+
+    private fun setExpanded(expanded: Boolean) = binding.infoContainer.updateLayoutParams {
+        height = if (expanded) LayoutParams.WRAP_CONTENT else 0
+        binding.expandImage.animate().rotationBy(-180f)
     }
 }
